d9acbe6a4dc91c5ddc871ebdff05e89ab9b01a9c
thach-le <lehoangthach2k2@gmail.com>
2025-07-28 18:32:19 +0700

    Update story API

--- api/stories/story.js
+++ api/stories/story.js
@@ -1,12 +1,6 @@
-import { getAllStories, addStory } from '../../controllers/storyController.js';
+const { getAllStories, addStory } = require('../controllers/storyController');
 
-export const config = {
-  api: {
-    bodyParser: true,
-  },
-};
-
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   try {
     if (req.method === 'GET') {
       const stories = await getAllStories();
@@ -23,4 +17,24 @@ export default async function handler(req, res) {
   } catch (err) {
     return res.status(500).json({ error: err.message });
   }
-}
+};
+const { getAllStories, addStory } = require('../controllers/storyController');
+
+module.exports = async function handler(req, res) {
+  try {
+    if (req.method === 'GET') {
+      const stories = await getAllStories();
+      return res.status(200).json(stories);
+    }
+
+    if (req.method === 'POST') {
+      const { title, url, img } = req.body;
+      const story = await addStory({ title, url, img });
+      return res.status(201).json(story);
+    }
+
+    return res.status(405).json({ error: 'Method Not Allowed' });
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+};
