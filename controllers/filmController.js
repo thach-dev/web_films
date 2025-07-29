52125b51f4222fe1c5bd2143974e3b0b8119a72a
thach-le <lehoangthach2k2@gmail.com>
2025-07-29 14:14:57 +0700

    up code

--- controllers/filmController.js
+++ controllers/filmController.js
@@ -3,9 +3,9 @@ import {
   getFilmByIdModel,
 } from '../models/filmModel.js';
 
-export async function getAllFilms() {
-  return await getAllFilmsModel();
-}
+// export async function getAllFilms() {
+//   return await getAllFilmsModel();
+// }
 
 export async function getFilmById(id) {
   return await getFilmByIdModel(id);
@@ -13,4 +13,14 @@ export async function getFilmById(id) {
 
 export async function addFilm({ title, url, img }) {
   return await addFilmModel({ title, url, img });
+}
+export async function getAllFilms() {
+  try {
+    const result = await pool.query('SELECT * FROM "list_video"');
+    console.log("GET ALL FILMS RESULT:", result.rows);
+    return result.rows;
+  } catch (error) {
+    console.error("Error fetching films:", error);
+    throw error;
+  }
 }
