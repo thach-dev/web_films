966be3e0f27d8f5342ad665249a0dc7e11dadadf
thach-le <lehoangthach2k2@gmail.com>
2026-02-23 05:30:03 +0700

    5:29  23/2/2026

--- models/bannerModel.js
+++ models/bannerModel.js
@@ -1,9 +1,6 @@
 import { supabase } from '../utils/cors.js';
 export async function getBannerModel() {
-  const { data, error } = await supabase
-    .from('banner')
-    .select('*')
-    .order('id', { ascending: true });
+  const { data, error } = await supabase.from('banner').select('*').order('id', { ascending: true });
 
   if (error) throw new Error(error.message);
   return data;
