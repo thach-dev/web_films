c2f4c33267cc2d951e9921721c96159fb16c6f46
thach-le <lehoangthach2k2@gmail.com>
2026-02-28 17:31:03 +0700

    up comment

--- api/comments/comments.js
+++ api/comments/comments.js
@@ -1,5 +1,6 @@
 import runCors from "../../utils/cors.js";
 import { CommentController } from "../../controllers/commentController.js";
+import { verifyToken } from "../../utils/verifyToken.js";
 
 export default async function handler(req, res) {
   const ended = runCors(req, res);
@@ -10,10 +11,26 @@ export default async function handler(req, res) {
   }
 
   if (req.method === "POST") {
+    const user = verifyToken(req);
+
+    if (!user) {
+      return res.status(401).json({ message: "Chưa đăng nhập" });
+    }
+
+    req.user = user; 
+
     return CommentController.addComment(req, res);
   }
 
   if (req.method === "DELETE") {
+    const user = verifyToken(req);
+
+    if (!user) {
+      return res.status(401).json({ message: "Chưa đăng nhập" });
+    }
+
+    req.user = user;
+
     return CommentController.deleteComment(req, res);
   }
 
