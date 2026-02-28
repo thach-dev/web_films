af6efbc7a54feee583e3fc5c890772a7f74e131b
thach-le <lehoangthach2k2@gmail.com>
2026-02-28 17:33:18 +0700

    tien het co the kiem lai

--- api/comments/comments.js
+++ api/comments/comments.js
@@ -1,6 +1,5 @@
 import runCors from "../../utils/cors.js";
 import { CommentController } from "../../controllers/commentController.js";
-import { verifyToken } from "../../utils/verifyToken.js";
 
 export default async function handler(req, res) {
   const ended = runCors(req, res);
@@ -11,26 +10,10 @@ export default async function handler(req, res) {
   }
 
   if (req.method === "POST") {
-    const user = verifyToken(req);
-
-    if (!user) {
-      return res.status(401).json({ message: "Chưa đăng nhập" });
-    }
-
-    req.user = user; 
-
     return CommentController.addComment(req, res);
   }
 
   if (req.method === "DELETE") {
-    const user = verifyToken(req);
-
-    if (!user) {
-      return res.status(401).json({ message: "Chưa đăng nhập" });
-    }
-
-    req.user = user;
-
     return CommentController.deleteComment(req, res);
   }
 
